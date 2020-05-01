5e47b53c1a9b4cdeec8c83ac5df41f632969329c
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-01 18:53:19 +0530

    Calculating Wage for Month

--- empSalary.sh
+++ empSalary.sh
@@ -3,21 +3,26 @@ printf "Welcome To Wage Computation Program"
 fulltime=2
 parttime=1
 absent=0
-observation=$((RANDOM%3))
 perHourWage=20
 fulltimeHrs=8
 parttimeHrs=4
-case $observation in
-	0)
-		empHrs=0
-		;;
-	1)
-		empHrs=4
-		;;
-	2)
-		empHrs=8
-		;;
-	*)
-		echo invalid input
-		;;
-esac
+for (( i=1; i<=20; i++ ))
+do
+	observation=$((RANDOM%3))
+	case $observation in
+		0)
+			empHrs=0
+			;;
+		1)
+			empHrs=4
+			;;
+		2)
+			empHrs=8
+			;;
+		*)
+			echo invalid input
+			;;
+	esac
+	dailyWage=$(($empHrs*$perHourWage))
+	totalSalary=$(($totalSalary+$dailyWage))
+done
